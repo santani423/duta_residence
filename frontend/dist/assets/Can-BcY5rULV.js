f9a26389930676802057d359cdfa0d6ba5fc56b4
santani423 <santani423@gmail.com>
2026-07-10 03:11:04 +0700

    penyesuaian api

--- frontend/dist/assets/Can-BcY5rULV.js
+++
@@ -1,1 +0,0 @@
-import{u as h}from"./index-GfEjh5zd.js";function A({permission:t,any:r,all:u,fallback:n=null,children:e}){const{can:o,canAny:c,canAll:f}=h();return t&&!o(t)||r?.length&&!c(r)||u?.length&&!f(u)?n:e}export{A as C};
